929927e0f7ea9b593f8ec95ccff52dae51f17515
Kevin Kane <kevinthomaskane@gmail.com>
2018-03-02 13:53:30 -0800

    added functionality to artist search

--- Assets/app.js
+++ Assets/app.js
@@ -38,6 +38,10 @@ $(document).on("click", "#addFavorite", function () {
     console.log(JSON.parse(localStorage.getItem("playlistSong")))
 })
 
+    function createChips(){
+
+    }
+
 var arrayOfCommunity = [];
 
 $(document).on("click", "#shareCommunity", function () {
@@ -48,18 +52,18 @@ $(document).on("click", "#shareCommunity", function () {
     localStorage.setItem("communitySong", JSON.stringify(arrayOfCommunity))
 })
 
-var searchParameter
+var searchParameter = "Artist";
 var clicked = false;
 
 if (clicked === false) {
-    $("#sideSearch").attr("disabled", "disabled");
+    console.log("false")
+    $(".input-field").hide();
 } else {
     $("#sideSearch").removeAttr("disabled");
 }
 
 
 $(document).on("click", "#artistSearch", function () {
-    searchParameter = $(this).text();
     clicked = true;
 })
 
@@ -73,11 +77,13 @@ $(document).on("click", "#songSearch", function () {
 $(document).on("click", "#searchSubmit", function () {
     var userInput = $("#sideSearch").val();
     console.log(userInput)
-    if (searchParameter === "artist") {
+    console.log(searchParameter)
+    if (searchParameter === "Artist") {
+        console.log("made it into if")
         artistHits(userInput)
     }
 
-    if (searchParameter === "song") {
+    if (searchParameter === "Title") {
         songHits(userInput)
     }
 })
@@ -107,6 +113,7 @@ function genreHits(search) {
 }
 
 function artistHits(search) {
+    console.log("hello")
     $.ajax({
         method: "GET",
         url: "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=" + search + "&api_key=6efca9dcca0f53fefbaf77e99b6dddf2&format=json"
@@ -120,15 +127,16 @@ function artistHits(search) {
                  <li class="resultList">
                     <div class="collapsible-header truncate"><img src="${obj[Object.keys(obj)[0]]}">${data.toptracks.track[i].name} - ${data.toptracks.track[i].artist.name}</div>
                      <div class="collapsible-body">
-                     <a href="#"><i class="fab fa-youtube"></i><span id="playYoutube"> Play YouTube video</span></a> <br>
-                     <a href="#"><i class="far fa-star"></i> <span id="addFavorite">Add to your favorites </span></a><br>
-                     <a href="#"><i class="fas fa-share-alt"></i> <span id="shareCommunity">Share with community </span></a>
+                     <a id="playYoutube" href="#"><i class="fab fa-youtube"></i>Play YouTube video</a> <br>
+                     <a id="addFavorite" href="#"><i class="far fa-star"></i>Add to your favorites</a><br>
+                     <a id="shareCommunity" href="#"><i class="fas fa-share-alt"></i>Share with community</a>
                      </div>
                 </li>     
              `)
         }
     })
 }
+songHits("humble")
 
 function songHits(search) {
     $.ajax({
@@ -138,11 +146,11 @@ function songHits(search) {
         $(".collapsible").empty();
         $(".collapsible").show();
         console.log(data)
-        for (let i = 0; i < data.tracks.track.length; i++) {
-            var obj = data.tracks.track[i].image[2];
+        for (let i = 0; i < data.results.trackmatches.track.length; i++) {
+            var obj = data.results.trackmatches.track[i].image[2];
             $(".collapsible").append(`
                  <li class="resultList">
-                    <div class="collapsible-header truncate"><img src="${obj[Object.keys(obj)[0]]}">${data.tracks.track[i].name} - ${data.tracks.track[i].artist.name}</div>
+                    <div class="collapsible-header truncate"><img src="${obj[Object.keys(obj)[0]]}">${data.results.trackmatches.track[i].name} - ${data.results.trackmatches.track[i].artist}</div>
                      <div class="collapsible-body">
                      <a id="playYoutube" href="#"><i class="fab fa-youtube"></i> Play YouTube video</a> <br>
                      <a id="addFavorite" href="#"><i class="far fa-star"></i>Add to your favorites </a><br>
